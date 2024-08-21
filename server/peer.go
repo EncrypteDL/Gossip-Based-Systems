3c7212932e8b8ca631a0b42a75f0345c40403e27
zacksfF <ssssaasss571@gmail.com>
2024-08-21 09:51:48 +0100

    zack fix Peer and Server

--- server/peer.go
+++ server/peer.go
@@ -1,1 +1,63 @@
 package server
+
+import (
+	"EncrypteDL/Gossip-Based-Systems/internal"
+	"errors"
+)
+
+var NoCorrectPeerAvailable = errors.New("There are no correct peers available")
+
+type PeerSet struct {
+	peer []*P2PClient
+}
+
+func (p *PeerSet) AddPeer(IPAddress string, portNumber int, connectionCount int) error {
+
+	client, err := NewClient(IPAddress, portNumber, connectionCount)
+	if err != nil {
+		return err
+	}
+
+	// starts the main loop of client
+	go client.Start()
+
+	p.peer = append(p.peer, client)
+
+	return nil
+}
+
+func (p *PeerSet) DissaminateChunks(chunks []internal.Chunk) {
+	for index, chunk := range chunks {
+		peer := p.selectPeer(index)
+		peer.SendBlockChunk(chunk)
+	}
+}
+
+func (p *PeerSet) ForwardChunk(chunk internal.Chunk) {
+
+	forwardCount := 0
+	for _, peer := range p.peer {
+		if peer.err != nil {
+			continue
+		}
+		forwardCount++
+		peer.SendBlockChunk(chunk)
+	}
+
+	if forwardCount == 0 {
+		panic(NoCorrectPeerAvailable)
+	}
+}
+
+func (p *PeerSet) selectPeer(index int) *P2PClient {
+
+	peerCount := len(p.peer)
+	for i := 0; i < peerCount; i++ {
+		peer := p.peer[(index+i)%peerCount]
+		if peer.err == nil {
+			return peer
+		}
+	}
+
+	panic(NoCorrectPeerAvailable)
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,19 @@
 package server
+
+import "EncrypteDL/Gossip-Based-Systems/internal"
+
+type P2PServer struct{
+	demux *internal.Demux
+}
+
+func NewServer(demux *internal.Demux) *P2PServer {
+	server := &P2PServer{demux: demux}
+	return server
+}
+
+func (s *P2PServer) HandleBlockChunk(chunk *internal.Chunk, reply *int) error {
+
+	s.demux.EnqueBlockChunk(*chunk)
+
+	return nil
+}
